e0046c85d746bbecf8916f93a978914eac108013
Valentyn KYCHAN <valentyn_kychan2107@tntu.edu.ua>
2026-07-07 23:24:53 +0200

    Create Project entity, test db connection

---
+++ src/Projects.Api/Models/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace Projects.Api.Models {
+    public class Project {
+        public Guid id { get; set; }
+        public string? name { get; set; }
+        public string? description { get; set; }
+        public bool isArchived { get; set; }
+        public DateTimeOffset? createdAt { get; set; }
+    }
+}
--- src/Projects.Api/Program.cs
+++ src/Projects.Api/Program.cs
@@ -1,11 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Cosmos;
+using Projects.Api.Models;
 
+var builder = WebApplication.CreateBuilder(args);
+var cosmosEndpoint = builder.Configuration["CosmosDb:Endpoint"];
+var cosmosKey = builder.Configuration["CosmosDb:Key"];
+var databaseName = builder.Configuration["CosmosDb:DatabaseName"];
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ProjectContext>(opt =>
+opt.UseCosmos(cosmosEndpoint, cosmosKey, databaseName));
 
 var app = builder.Build();
 
@@ -13,6 +21,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+    try {
+        var context = services.GetRequiredService<ProjectContext>();
+        await context.Database.EnsureCreatedAsync();
+        Console.WriteLine("Success, connected to Cosmos DB!");
+    } catch (Exception ex) {
+        Console.WriteLine($" Error inializing from the start: {ex.Message}");
+    }
 }
 
 app.UseHttpsRedirection();
