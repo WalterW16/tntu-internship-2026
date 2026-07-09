12d9a2fe8c1726d715f09b822cc1bb79db41312f
Valentyn KYCHAN <138904407+WalterW16@users.noreply.github.com>
2026-07-09 14:58:54 +0200

    Projects.api scaffold (#1)
    * Create projects and cofigure gitignore
    
    * Create Project entity, test db connection

---
+++ src/Projects.Api/Models/ProjectContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Projects.Api.Models {
+    public class ProjectContext : DbContext {
+        public ProjectContext(DbContextOptions<ProjectContext> options)
+            : base(options) { }
+        public DbSet<Project> Projects { get; set; } = null;
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Project>().ToContainer("projects").HasPartitionKey(p => p.id);
+        }
+    }
+
+}
