ddd7a45cdfe57e29d634c96e558673aa5d79b0ff
Valentyn KYCHAN <valentyn_kychan2107@tntu.edu.ua>
2026-07-17 09:14:47 +0200

    Add missing integration tests for us-001

---
+++ src/Projects.Api.Tests/unit/ProjectServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+
+namespace Projects.Api.Tests.unit {
+    public class ProjectServiceTests {
+        [Fact]
+        public void __() {
+
+        }
+    }
+}
