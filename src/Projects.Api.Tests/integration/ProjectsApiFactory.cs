6546bde988600d56033050b7856d90603952223a
Valentyn KYCHAN <valentyn_kychan2107@tntu.edu.ua>
2026-07-25 15:46:29 +0200

    Test fix

--- src/Projects.Api.Tests/integration/ProjectsApiFactory.cs
+++ src/Projects.Api.Tests/integration/ProjectsApiFactory.cs
@@ -6,24 +6,31 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Projects.Api.Data;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Projects.Api.Tests.integration {
     public class ProjectsApiFactory : WebApplicationFactory<Program> {
         protected override void ConfigureWebHost(IWebHostBuilder builder) {
+
             builder.ConfigureAppConfiguration((context, config) => {
-                _ = config.AddInMemoryCollection(new Dictionary<string, string>
-                {{ "APPLICATIONINSIGHTS_CONNECTION_STRING", "InstrumentationKey=00000000-0000-0000-0000-000000000000;" }});
+                // Додано знак питання до string? щоб уникнути попередження CS8620
+                _ = config.AddInMemoryCollection(new Dictionary<string, string?>
+                {
+                    { "APPLICATIONINSIGHTS_CONNECTION_STRING", "InstrumentationKey=00000000-0000-0000-0000-000000000000;" }
+                });
             });
             builder.ConfigureServices(services => {
-                    var descriptor = services.SingleOrDefault(
+                services.Configure<Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration>(config => {
+                    config.DisableTelemetry = true;
+                });
+                var descriptor = services.SingleOrDefault(
                     d => d.ServiceType == typeof(DbContextOptions<ProjectContext>));
-
                 if (descriptor != null) {
                     services.Remove(descriptor);
                 }
                 string dbName = $"InMemoryDbForTesting_{Guid.NewGuid()}";
-                    services.AddDbContext<ProjectContext>(options => {
+                services.AddDbContext<ProjectContext>(options => {
                     options.UseInMemoryDatabase(dbName);
                 });
             });
